8671a7131c37f62e4c938943a827fba541184df3
Omar IZEM <contact@izem.dev>
2023-03-04 00:49:47 +0100

    test(InMemoryDomainEventPublisher): update tests to support latest changes in event publishing and handling algorithm

--- app/__tests__/shared/domain/events/in-memory-domain-event-publisher.spec.ts
+++ app/__tests__/shared/domain/events/in-memory-domain-event-publisher.spec.ts
@@ -3,72 +3,116 @@ import { UniqueUUID } from '@shared/domain/value-objects/UniqueUUID';
 import { IDomainEvent } from '@shared/domain/building-blocks/IDomainEvent';
 import { IDomainEventHandler } from '@shared/domain/interfaces/IDomainEventHandler';
 import { DateTimeValueObject } from '@shared/domain/value-objects/DateTimeValueObject';
+import { DomainEventBase } from '@shared/domain/events/DomainEventBase';
+
+const mockDomainEvent = (
+  aggregateId: UniqueUUID,
+  name?: string
+): IDomainEvent => {
+  return {
+    _id: UniqueUUID.generate(),
+    _eventName: name ?? 'MockEvent',
+    _occurredAt: DateTimeValueObject.now(),
+    get id(): UniqueUUID {
+      return this._id;
+    },
+    get name(): string {
+      return this._eventName;
+    },
+    get occurredAt(): DateTimeValueObject {
+      return this._occurredAt;
+    },
+    get aggregateRootId(): UniqueUUID {
+      return aggregateId;
+    },
+    equals(other: IDomainEvent): boolean {
+      return (
+        this.id.equals(other.id) &&
+        this.name === other.name &&
+        this.aggregateRootId.equals(other.aggregateRootId)
+      );
+    }
+  };
+};
 
 describe('InMemoryDomainEventPublisher', () => {
   let publisher: InMemoryDomainEventPublisher;
-  const mockEvent: IDomainEvent = {
-    eventName: 'MockEvent',
-    occurredAt: DateTimeValueObject.now(),
-    aggregateRootId: new UniqueUUID()
-  };
+  let aggregateRootId: UniqueUUID;
+
+  const mockEvent: IDomainEvent = mockDomainEvent(aggregateRootId);
 
   beforeEach(() => {
     publisher = InMemoryDomainEventPublisher.getInstance();
+    aggregateRootId = UniqueUUID.generate();
   });
 
   afterEach(() => {
     publisher.clearAggregateRootEvents(mockEvent.aggregateRootId);
   });
 
-  it('should be able to publish events', async () => {
-    const aggregateRootId = mockEvent.aggregateRootId;
-    await publisher.publish(aggregateRootId, mockEvent);
-    const aggregateRootEvents =
-      publisher.getAggregateRootEvents(aggregateRootId);
-
-    expect(aggregateRootEvents).toHaveLength(1);
+  it('InMemoryDomainEventPublisher should be a singleton', () => {
+    const publisher2 = InMemoryDomainEventPublisher.getInstance();
+    expect(publisher).toBe(publisher2);
   });
 
-  it('should be able to clear events', async () => {
-    const aggregateRootId = mockEvent.aggregateRootId;
-    await publisher.publish(aggregateRootId, mockEvent);
-    publisher.clearAggregateRootEvents(aggregateRootId);
-    const aggregateRootEvents =
-      publisher.getAggregateRootEvents(aggregateRootId);
-    expect(aggregateRootEvents).toHaveLength(0);
+  it('should publish an event', async () => {
+    await publisher.publish(mockEvent);
+    expect(publisher.getAggregateRootEvents(aggregateRootId).length).toBe(1);
   });
 
-  it('should be able to register and handle events', async () => {
-    const mockHandler: IDomainEventHandler<IDomainEvent> = {
-      handle: jest.fn(),
-      subscribeTo: jest.fn()
-    };
-    publisher.registerHandler(mockHandler, mockEvent.eventName);
-
-    const aggregateRootId = new UniqueUUID();
-    await publisher.publish(aggregateRootId, mockEvent);
-    await publisher.dispatchAggregateRootEvents(aggregateRootId);
-
-    expect(mockHandler.handle).toHaveBeenCalledTimes(1);
-    expect(mockHandler.handle).toHaveBeenCalledWith(mockEvent);
+  it('should publish an event only once', async () => {
+    await publisher.publish(mockEvent);
+    await publisher.publish(mockEvent);
+    expect(publisher.getAggregateRootEvents(aggregateRootId).length).toBe(1);
   });
 
-  it('should dispatch event by name', async () => {
-    const mockHandler: IDomainEventHandler<IDomainEvent> = {
-      handle: jest.fn(),
-      subscribeTo: jest.fn()
-    };
-
-    publisher.registerHandler(mockHandler, mockEvent.eventName);
-
-    const aggregateRootId = new UniqueUUID();
-    await publisher.publish(aggregateRootId, mockEvent);
-    await publisher.dispatchAggregateRootEventByName(
+  it('should get all events for an aggregate root', async () => {
+    await publisher.publish(mockEvent);
+    const mockEvent2: DomainEventBase = mockDomainEvent(
       aggregateRootId,
-      mockEvent.eventName
+      'MockEvent2'
     );
 
-    expect(mockHandler.handle).toHaveBeenCalledTimes(1);
-    expect(mockHandler.handle).toHaveBeenCalledWith(mockEvent);
+    await publisher.publish(mockEvent2);
+    expect(publisher.getAggregateRootEvents(aggregateRootId).length).toBe(2);
+  });
+
+  it('should clear all events for an aggregate root', async () => {
+    await publisher.publish(mockEvent);
+    publisher.clearAggregateRootEvents(aggregateRootId);
+    expect(publisher.getAggregateRootEvents(aggregateRootId).length).toBe(0);
+  });
+
+  it('should register a handler for an event', () => {
+    const mockHandler: IDomainEventHandler<IDomainEvent> = {
+      handle: jest.fn(),
+      subscribeTo: jest.fn()
+    };
+    publisher.registerHandler(mockHandler, mockEvent.name);
+    expect(publisher.handlers.size).toBe(1);
+    expect(publisher.handlers.get(mockEvent.name).length).toBe(1);
+  });
+
+  it('should dispatch all events for an aggregate root', async () => {
+    const mockHandler: IDomainEventHandler<IDomainEvent> = {
+      handle: jest.fn(),
+      subscribeTo: jest.fn()
+    };
+    publisher.registerHandler(mockHandler, mockEvent.name);
+    await publisher.publish(mockEvent);
+    await publisher.dispatchAggregateRootEvents(aggregateRootId);
+    expect(mockHandler.handle).toBeCalledTimes(1);
+  });
+
+  it('should dispatch all events for an aggregate root only once', async () => {
+    const mockHandler: IDomainEventHandler<IDomainEvent> = {
+      handle: jest.fn(),
+      subscribeTo: jest.fn()
+    };
+    publisher.registerHandler(mockHandler, mockEvent.name);
+    await publisher.publish(mockEvent);
+    await publisher.dispatchAggregateRootEvents(aggregateRootId);
+    await publisher.dispatchAggregateRootEvents(aggregateRootId);
+    expect(mockHandler.handle).toBeCalledTimes(1);
   });
 });
